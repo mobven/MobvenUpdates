57ea67028b419c28ddbb9345bca755ba50c02210
Rasid Ramazanov <rasid.ramazanov@mobven.com>
2019-09-25 23:31:23 +0300

    Added analytics names

---
+++ MobvenUpdates+RemoteConfig/MobvenUpdatesAnalyticsNames.swift
@@ -0,0 +1,9 @@
+//
+//  MobvenUpdatesAnalyticsNames.swift
+//  MobvenUpdates+RemoteConfig
+//
+//  Created by Rasid Ramazanov on 25.09.2019.
+//  Copyright © 2019 Mobven Technology. All rights reserved.
+//
+
+import Foundation
